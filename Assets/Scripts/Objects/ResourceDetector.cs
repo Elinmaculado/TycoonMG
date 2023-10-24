b08d8565c3eeec291d2f797c627e58d890ed5ac9
Miguel239g <127420871+Elinmaculado@users.noreply.github.com>
2023-10-24 08:54:38 -0600

    Main menu

--- Assets/Scripts/Objects/ResourceDetector.cs
+++ Assets/Scripts/Objects/ResourceDetector.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ResourceDetector : MonoBehaviour
 {
     // Start is called before the first frame update
